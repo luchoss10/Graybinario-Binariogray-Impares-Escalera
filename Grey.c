e7fec9ee90342aa60b81264f59b2171f55179b76
luchoss10 <lucncd@gmail.com>
2018-09-05 11:34:07 -0500

    Update Grey.c

--- Grey.c
+++ Grey.c
@@ -18,12 +18,12 @@ void main(){
          In1=INPUT(PIN_A1);
          In2=INPUT(PIN_A2);                 
          In3=INPUT(PIN_A3); 
-         //Empiezo a hacer las acciones correspondientes para optener el codigo gray
+         //Empiezo a hacer las acciones correspondientes para obtener el codigo gray
          Out3=In3; //El ultimo numero de derecha a izquierda se conserva
          Out2=In3^In2; //El  tercer valor de salida corresponde a una XOR entre In3 e In2
          Out1=In2^In1; //El  segundo valor de salida corresponde a una XOR entre In2 e In1
          Out0=In1^In0; //El primer valor de salida corresponde a una XOR entre In1 e In0
-         Dato=Out3*8+Out2*4+Out1*2+Out0*1;//Le doy una interpretacion numerica al valor de salida para que sea m·s facil de mostrar
+         Dato=Out3*8+Out2*4+Out1*2+Out0*1;//Le doy una interpretacion numerica al valor de salida para que sea m√°s facil de mostrar
          output_d(Dato); //Muestro el valor obtenido      
       }                                   
       else if(bit_test(e,4)==0){                      
@@ -31,12 +31,12 @@ void main(){
          In1=INPUT(PIN_A1);
          In2=INPUT(PIN_A2);                 
          In3=INPUT(PIN_A3);
-         //Empiezo a hacer las acciones correspondientes para optener el codigo gray
+         //Empiezo a hacer las acciones correspondientes para obtener el codigo gray
          Out3=In3; //El ultimo numero de derecha a izquierda se conserva
          Out2=In3^In2; //El  tercer valor de salida corresponde a una XOR entre In3 e In2
          Out1=Out2^In1; //El  segundo valor de salida corresponde a una XOR entre In2 e In1
          Out0= Out1^In0; //El primer valor de salida corresponde a una XOR entre In1 e In0
-         Dato=Out3*8+Out2*4+Out1*2+Out0*1;//Le doy una interpretacion numerica al valor de salida para que sea m·s facil de mostrar
+         Dato=Out3*8+Out2*4+Out1*2+Out0*1;//Le doy una interpretacion numerica al valor de salida para que sea m√°s facil de mostrar
          output_d(Dato); //Muestro el valor obtenido                       
       }                       
    }
